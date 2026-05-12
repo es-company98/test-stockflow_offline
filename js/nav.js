220ae508d0328c4dfc4da006d389a317959b6584
es-company98 <escompany98@gmail.com>
2026-05-12 14:02:13 +0200

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,14 @@
  // NAV
-  document.querySelectorAll('nav .nav-item').forEach(item => {
-    if (item.dataset.page === location.pathname.split('/').pop()) {
-      item.classList.add('active');
+  const currentPage =
+  location.pathname.split("/").pop();
+
+document
+  .querySelectorAll(".nav-item")
+  .forEach(item => {
+
+    if (
+      item.dataset.page === currentPage
+    ) {
+      item.classList.add("active");
     }
-    item.addEventListener('click', () => location.href = item.dataset.page);
-  });
-
-  // DATE TOGGLE
-  const manualDateCheckbox = document.getElementById('manualDate');
-  const saleDateInput = document.getElementById('saleDate');
-
-  manualDateCheckbox.addEventListener('change', () => {
-    saleDateInput.disabled = !manualDateCheckbox.checked;
   });
