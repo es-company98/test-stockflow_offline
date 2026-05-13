3f8ef95c3c2b68f7ccaa97676970fb00ad013785
es-company98 <escompany98@gmail.com>
2026-05-13 22:08:46 +0200

    Update ranging.js

--- js/ranging.js
+++ js/ranging.js
@@ -13,7 +13,7 @@ import {
 import {
   getAuth,
   onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 
 /* =========================
    DOM
