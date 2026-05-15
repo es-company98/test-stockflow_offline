9597f68f7ee574309f73f8ae59c320d9b2aca9eb
es-company98 <escompany98@gmail.com>
2026-05-15 16:13:45 +0200

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,26 +1,71 @@
-// firebase.js
+// firebase.js offline 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-import { 
-  getFirestore, collection, addDoc, getDocs,setDoc, updateDoc, deleteDoc, doc,
-  getDoc, query, where, orderBy, serverTimestamp, Timestamp, enableIndexedDbPersistence,
-  runTransaction, limit, writeBatch, increment, arrayUnion
+
+import {
+  initializeFirestore,
+  persistentLocalCache,
+  persistentMultipleTabManager,
+
+  collection,
+  addDoc,
+  getDocs,
+  setDoc,
+  updateDoc,
+  orderBy,
+  deleteDoc,
+  doc,
+  getDoc,
+  query,
+  where,
+  serverTimestamp,
+  Timestamp,
+  runTransaction,
+  writeBatch,
+  increment,
+  arrayUnion,
+  limit
+
 } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAwY95NM8QKiRxNHvfxiGrjvhKG16Lpl5E",
-    authDomain: "test-stockflow-offline.firebaseapp.com",
-    projectId: "test-stockflow-offline",
-    storageBucket: "test-stockflow-offline.firebasestorage.app",
-    messagingSenderId: "721689942624",
-    appId: "1:721689942624:web:a29d362e5fccd7e2ce4105",
-    measurementId: "G-67C96ZB39P"
-  };
+  apiKey: "AIzaSyAwY95NM8QKiRxNHvfxiGrjvhKG16Lpl5E",
+  authDomain: "test-stockflow-offline.firebaseapp.com",
+  projectId: "test-stockflow-offline",
+  storageBucket: "test-stockflow-offline.firebasestorage.app",
+  messagingSenderId: "721689942624",
+  appId: "1:721689942624:web:a29d362e5fccd7e2ce4105",
+  measurementId: "G-67C96ZB39P"
+};
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-export { 
-  db, collection, addDoc, getDocs, updateDoc, orderBy, deleteDoc, doc, getDoc,setDoc,
-  query, where, serverTimestamp, Timestamp, enableIndexedDbPersistence,
-  runTransaction, limit, writeBatch, increment, arrayUnion
+const db = initializeFirestore(app, {
+  localCache: persistentLocalCache({
+    tabManager: persistentMultipleTabManager()
+  })
+});
+
+const enableIndexedDbPersistence = async () => true;
+
+export {
+  db,
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  doc,
+  getDoc,
+  setDoc,
+  query,
+  where,
+  serverTimestamp,
+  Timestamp,
+  runTransaction,
+  limit,
+  orderBy,
+  writeBatch,
+  increment,
+  arrayUnion,
+  enableIndexedDbPersistence
 };
